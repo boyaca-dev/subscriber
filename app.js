f6b523ed526b21fc75f883ebce69edaaa5591bc0
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2019-07-29 14:47:27 -0500

    Update to use netlify url

--- app.js
+++ app.js
@@ -6,14 +6,15 @@ const bodyParser = require('body-parser');
 const apiRoutes = require('./routes/api');
 const subRoutes = require('./routes/subscriber');
 
+const netlifyFuncs = '/.netlify/functions/server';
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/api', apiRoutes);
-app.use('/api/subscribe', subRoutes);
+app.use(`${netlifyFuncs}/api`, apiRoutes);
+app.use(`${netlifyFuncs}/api/subscribe`, subRoutes);
 
 module.exports = app;
 module.exports.handler = serverless(app);
