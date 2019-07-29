077d9b4273269a9cf721a1e8f4be563c8f8e25fe
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2019-07-29 15:53:44 -0500

    Initial setup for mailer lite api

--- routes/subscriber.js
+++ routes/subscriber.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const axios = require('axios');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  const mailerGroup = process.env.MAILER_LITE_GROUP || '';
+  if (mailerGroup.length <= 0) {
+    return res.send({ success: false, error: 'Invalid MailerLite group' });
+  }
+
+  const mailerKey = process.env.MAILER_LITE_API || '';
+  if (mailerKey.length <= 0) {
+    return res.send({ success: false, error: 'Invalid MailerLite api key' });
+  }
+
   const { name, email } = req.body;
-  console.log(name, email);
-  return res.send(true);
+  const data = { name, email };
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+      'X-MailerLite-ApiKey': mailerKey,
+    }
+  };
+  return axios.post(`https://api.mailerlite.com/api/v2/groups/${mailerGroup}/subscribers`,
+    data, config)
+    .then(result => res.send({ success: true, data: result.data }))
+    .catch(err => {
+      return res.send({ success: false, error: err ? err.message || err.toString() : 'Error' });
+    });
 });
 
 module.exports = router;
